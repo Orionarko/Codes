ec7e2ab3cf763cdbbaa47ac5da410493b023ff9e
Orionarko <43388615+Orionarko@users.noreply.github.com>
2018-10-06 19:32:20 +0530

    Update Insertion Sort.cpp

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -12,7 +12,7 @@ cout<<"Sorting an array using Insertion Sort\n";
 for(i=0; i<size; i++){
 temp=arr[i];
 j=i-1;
-while((temp<arr[j] && (j>=0)){
+while(temp<arr[j] && (j>=0)){
 arr[j+1]=arr[j];
 j=j-1;
 }
@@ -20,6 +20,6 @@ arr[j+1]=temp;
 }
 cout<<"Array after sorting:\n";
 for(i=0; i<size; i++){
-cout<<arr[i]<<""
+cout<<arr[i]<<"";
 }
 }
